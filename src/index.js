7686a151f846d1eb0fc4c2faaa5c2152e8c319d7
Ayeong Kim <devayeong@gmail.com>
2022-03-06 22:56:37 +0900

    Register Page 진행 중

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import clayful from "clayful/client-js";
+import axios from 'axios';
+
+clayful.config({
+  client: "" //clayful api 토큰 복사 후 붙여넣기
+});
+
+clayful.install ('request', require('clayful/plugins/request-axios')(axios));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,34 @@
+import clayful from 'clayful';
 import React from 'react'
 
 function LandingPage() {
+
+  var Product = clayful.Product;
+
+var options = {
+    query: {
+        page: 1
+    }
+};
+
+Product.list(options, function(err, response) {
+
+    if (err) {
+        console.log(err.isclayful);
+        console.log(err.model);
+        console.log(err.method);
+        console.log(err.status);
+        console.log(err.headers);
+        console.log(err.code);
+        console.log(err.message);
+    }
+    
+    console.log(response.status);
+    console.log(response.headers);
+    console.log(response.data);
+
+});
+
   return (
     <div>LandingPage</div>
   )
