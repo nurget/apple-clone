f7f471d88ee94443a258eb349624e5bcb5af7aa8
Ayeong Kim <devayeong@gmail.com>
2022-03-08 22:21:03 +0900

    회원가입 기능 구현

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,13 +1,60 @@
-import React from 'react'
+import clayful from 'clayful/client-node';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var Customer = clayful.Customer;
+
+    var payload = {
+        email,
+        password
+    };
+
+    Customer.createMe(payload, function(err, result) {
+        if (err) {
+            // Error case
+            // Clayful API 비밀번호 8글자 이상 아니면 에러발생
+            console.log(err.code);
+        }
+
+        var data = result.data;
+
+        console.log(data);
+
+    });
+  }
+
   return (
     <div className='auth-wrapper'>
         <h1>회원가입</h1>
-        <form>
-            <input placeholder='Apple ID' type="email" name="email" value="" />
-            <input placeholder='암호' type="password" name="password" value="" />
+        <form onSubmit={handleSubmit}>
+            <input
+              onChange={handleEmailChange} 
+              placeholder='Apple ID' 
+              type="email" 
+              name="email" 
+              value={email} 
+            />
+            <input
+              onChange={handlePasswordChange} 
+              placeholder='암호' 
+              type="password" 
+              name="password" 
+              value={password}
+            />
 
             <button type='submit'>회원가입</button>
             <Link to="login" style={{ color: 'gray', textDecoration: "none" }}>
@@ -19,4 +66,4 @@ function RegisterPage() {
   );
 }
 
-export default RegisterPage
+export default RegisterPage;
